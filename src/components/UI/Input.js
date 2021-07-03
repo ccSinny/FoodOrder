201d2fb3d6bf28b8a185af667d1f40777dd11984
Chloe <chloe1c@outlook.com>
2021-07-03 09:16:02 +0100

    Cart context + refs

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,10 +1,13 @@
-import classes from './Input.module.css';
+import classes from "./Input.module.css";
+import React from "react";
 
-const Input = props => {
+const Input = React.forwardRef((props, ref) => {
+  return (
     <div className={classes.input}>
-        <label htmlFor={props.input.id}>{props.label}</label>
-        <Input {...props.input}/>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <Input ref={ref} {...props.input} />
     </div>
-}
+  );
+});
 
 export default Input;
---
+++ src/store/CartProvider.js
@@ -0,0 +1,45 @@
+import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+      const updatedItems = state.items.concat(action.item);
+      const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+      return {
+          items: updatedItems,
+          totalAmount: updatedTotalAmount,
+      };
+  }
+  return defaultCartState;
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return (
+    <CartContext.Provide value={cartContext}>
+      {props.children}
+    </CartContext.Provide>
+  );
+};
+
+export default CartProvider;
